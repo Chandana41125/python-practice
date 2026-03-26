c5457d4e5fd059ab9393284f853f30d823f637ce
chandana41125 <chandanachandu41125@gmail.com>
2026-03-26 08:43:12 +0530

    updated user_input_sum file

--- user_input_sum.py
+++ user_input_sum.py
@@ -3,12 +3,15 @@
 
 
 Total = 0
-
+i=1
+print("Sriram varma store")
 while True:
     userInput = input("Enter the Item price or press 'q' to quit: ")
+
     if userInput!='q':
         Total += int(userInput)
-        print(f"Order Total so far: {Total} ")
+        print(f"{i}. Order Total so far: {Total} ")
+        i+=1
     else:
         print(f"Your Bill total is {Total}. Thank you for shopping with us.")
         break
