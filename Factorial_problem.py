8aec32b8ebc4185b18b61acb0b94ea46a3fe5665
chandana41125 <chandanachandu41125@gmail.com>
2026-03-27 08:07:24 +0530

    Added factorial and count all zeros present in output basic implementation

---
+++ Factorial_problem.py
@@ -0,0 +1,16 @@
+
+def factorial(num):
+    fact = 1
+    for n in range(num,0,-1):
+        fact = fact * n
+    return fact
+
+
+res = factorial(int(input("Enter a number that you want to do factorial: ")))
+print(res)
+
+count = 0
+for i in str(res):
+    if i == '0':
+        count += 1
+print(int(count))
